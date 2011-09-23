85e23d22a1c4370ae004187c2bb9e884d014a16d
seraph <local@seraphr.jp>
2011-09-24 05:03:31 +0900

    Boot.scala書き換え

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -22,7 +22,7 @@ class Boot {
       val vendor = 
 	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
 			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+			     "jdbc:h2:mem:lift_proto.db.",
 			     Props.get("db.user"), Props.get("db.password"))
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
